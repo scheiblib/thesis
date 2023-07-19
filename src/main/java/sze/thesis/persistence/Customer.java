6c52ffdb0a41750d74f0947c5ddff06d03226045
Scheibli Bence <scheibli.bence@gmail.com>
2023-07-19 21:33:24 +0200

    first entity created

---
+++ src/main/java/sze/thesis/persistence/Customer.java
@@ -0,0 +1,38 @@
+package sze.thesis.persistence;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "customer")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id")
+    private long customerId;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "phone", nullable = false)
+    private String phone;
+
+    @Column(name = "city", nullable = false)
+    private String city;
+    @Column(name = "address", nullable = false)
+    private String address;
+}
