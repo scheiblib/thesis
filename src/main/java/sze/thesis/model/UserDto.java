7edbb4b27fab6dffa2a3dae08f352ea94ad6f3b5
Scheibli Bence <scheibli.bence@gmail.com>
2023-08-01 17:54:32 +0200

    userDto classes created

---
+++ src/main/java/sze/thesis/model/UserDto.java
@@ -0,0 +1,32 @@
+package sze.thesis.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class UserDto {
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String phone;
+    @NotBlank
+    private String city;
+    @NotBlank
+    private String address;
+}
