aa2287b068b3dd53e6fe0f2cd710e0929b72d5b3
Scheibli Bence <scheibli.bence@gmail.com>
2023-08-01 18:41:59 +0200

    user mapper added

--- src/main/java/sze/thesis/persistence/entity/User.java
+++ src/main/java/sze/thesis/persistence/entity/User.java
@@ -2,6 +2,7 @@ package sze.thesis.persistence.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import java.util.Set;
 
 @Entity
 @Table(name = "users")
+@Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
---
+++ src/main/java/sze/thesis/service/mapper/UserMapper.java
@@ -0,0 +1,48 @@
+package sze.thesis.service.mapper;
+
+import org.springframework.stereotype.Component;
+import sze.thesis.model.CreateUserDto;
+import sze.thesis.model.UserResponseDto;
+import sze.thesis.persistence.entity.Role;
+import sze.thesis.persistence.entity.User;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+public class UserMapper {
+    public User mapForUserRegister (CreateUserDto createUserDto){
+        return User.builder()
+                .firstName(createUserDto.getFirstName())
+                .lastName(createUserDto.getLastName())
+                .email(createUserDto.getEmail())
+                .password(createUserDto.getPassword())
+                .phone(createUserDto.getPhone())
+                .city(createUserDto.getCity())
+                .address(createUserDto.getAddress())
+                .roles(Collections.singleton(Role.USER))
+                .build();
+    }
+
+    public UserResponseDto mapUserEntityToUserResponseDto (User user){
+        return UserResponseDto.builder()
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .email(user.getEmail())
+                .phone(user.getPhone())
+                .city(user.getCity())
+                .address(user.getAddress())
+//                .role(SecurityContextHolder.getContext().getAuthentication().getName().toUpperCase())
+                .orders(user.getOrders())
+                .build();
+    }
+
+    public List<UserResponseDto> userResponseDtoList(List<User> userList){
+        List<UserResponseDto> userResponseDtoList = new ArrayList<>();
+        for(User user : userList){
+            userResponseDtoList.add(mapUserEntityToUserResponseDto(user));
+        }
+        return userResponseDtoList;
+    }
+}
