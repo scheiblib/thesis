c8c5c13ce87fbc0b551324ed8c68a0318e3c0109
Scheibli Bence <scheibli.bence@gmail.com>
2023-08-22 13:21:03 +0200

    item services and controller added

--- src/main/java/sze/thesis/service/ItemService.java
+++ src/main/java/sze/thesis/service/ItemService.java
@@ -2,11 +2,22 @@ package sze.thesis.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import sze.thesis.persistence.entity.Item;
 import sze.thesis.persistence.repository.ItemRepository;
+
+import java.util.List;
 
 @Service
 public class ItemService {
 
     @Autowired
     private ItemRepository itemRepository;
+
+    public Item findItemById(long id){
+        return itemRepository.findById(id);
+    }
+
+    public List<Item> findAll(){
+        return itemRepository.findAll();
+    }
 }
