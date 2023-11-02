92230eaf7b84e50aafa7314b77fd622e0b583d99
Scheibli Bence <scheibli.bence@gmail.com>
2023-11-02 20:21:20 +0100

    refactor: unused field, class

--- src/main/java/sze/thesis/persistence/repository/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package sze.thesis.persistence.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import sze.thesis.persistence.entity.Cart;
-
-@Repository
-public interface CartRepository extends JpaRepository<Cart, Long> {
-}
